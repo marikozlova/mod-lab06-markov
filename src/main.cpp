62d12f145e059dbfba880e3b3c7e1f1a96cbba55
marikozlova <162039344+marikozlova@users.noreply.github.com>
2024-04-21 23:37:00 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,8 @@
 int main() {
     const int NPREF = 2;
     const int MAXGEN = 1000;
-    
     std::ifstream input_stream("test.txt");
     std::string string, line;
-    
     while (std::getline(input_stream, line)) string += line + " ";
     Text_Gen textgen;
     textgen.Prefixes(string, NPREF);
