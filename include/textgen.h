f16302e1bacaa6229dd28ee1e1ad13855146d352
marikozlova <162039344+marikozlova@users.noreply.github.com>
2024-04-21 22:58:41 +0300

    Create textgen.h

---
+++ include/textgen.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <deque>
+#include <string>
+#include <map>
+#include <vector>
+
+typedef std::deque<std::string> prefix;
+
+class Text_Gen {
+ private:
+    std::map<prefix, std::vector<std::string>> statetab;
+    prefix f_prefix;
+ public:
+    void set(std::map<prefix, std::vector<std::string>> states);
+    void Prefixes(const std::string& inpstr, int prefnum);
+    std::map<prefix, std::vector<std::string>> get();
+    std::string Text(int generation_seed, int prefixes_amount);
+};
