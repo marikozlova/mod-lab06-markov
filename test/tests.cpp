066b650e627242fc4fef215aa569ad5f5950da36
marikozlova <162039344+marikozlova@users.noreply.github.com>
2024-04-21 23:26:46 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,7 +3,7 @@
 
 #include "../include/textgen.h"
 TEST(Test1, first_test) {
-    TextGen textgen;
+    Text_Gen textgen;
     textgen.Prefixes("One two three", 2);
     std::map<prefix, std::vector<std::string>> statetab = textgen.get();
     prefix pref = { "One", "two" };
@@ -11,14 +11,14 @@ TEST(Test1, first_test) {
 }
 
 TEST(Test2, second_test) {
-    TextGen textgen;
+    Text_Gen textgen;
     textgen.Prefixes("One two three", 2);
     prefix prefix = { "One", "two" };
     EXPECT_EQ(textgen.get().at(prefix)[0], "three");
 }
 
 TEST(Test3, third_test) {
-    TextGen textgen;
+    Text_Gen textgen;
     textgen.Prefixes("one two three four five six", 1);
     prefix pref = { "one" };
     std::vector<std::string> suffix = { "three", "five" };
@@ -26,7 +26,7 @@ TEST(Test3, third_test) {
 }
 
 TEST(Test4, fourth_test) {
-    TextGen textgen;
+    Text_Gen textgen;
     textgen.Prefixes("one two three four five six", 1);
     std::string generated_string = textgen.Text(time(0), 5);
     std::cout << generated_string;
